6e5c9245c94b5143d74257751206e9075746c9d8
Taylour <29444471+tailorem@users.noreply.github.com>
2018-07-09 23:45:37 +0000

    began writing functionality

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,12 @@
 var request = require("request");
 
+function getRepoContributors(repoOwner, repoName, cb) {
+
+}
+
+getRepoContributors("aseprite", "aseprite", function(err, response) {
+  console.log("Errors:", err);
+  console.log("Result:", response);
+});
+
 console.log("Welcome to the Github Avatar Downloader!");
