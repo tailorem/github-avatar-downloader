c828e2ce75b625cbe384986a5d9f36fd9a5a8aea
Taylour <29444471+tailorem@users.noreply.github.com>
2018-07-10 00:35:10 +0000

    added .gitignore

--- download_avatars.js
+++ download_avatars.js
@@ -1,11 +1,25 @@
 var request = require("request");
+var token = require("./secrets");
 
 function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
+    headers: {
+      "User-Agent": "request",
+      "Authorization": token
+    }
+  };
 
+  // var url = "https://api.github.com/repos/" + repoOwner + repoName + "/contributors";
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
 }
 
 getRepoContributors("aseprite", "aseprite", function(err, response) {
-  console.log("Errors:", err);
+  if (err) throw err;
+
+  // console.log("Error:", err);
   console.log("Result:", response);
 });
 
