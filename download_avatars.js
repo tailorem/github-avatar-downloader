43c3c8cedf1d437dddeb2550fc709ec78857cadb
Taylour <29444471+tailorem@users.noreply.github.com>
2018-07-10 00:35:55 +0000

    printed avatar_url s

--- download_avatars.js
+++ download_avatars.js
@@ -1,26 +1,32 @@
 var request = require("request");
-var token = require("./secrets");
+// require authorizations
+var secrets = require("./secrets");
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
     headers: {
       "User-Agent": "request",
-      "Authorization": token
+      "Authorization": secrets.GITHUB_TOKEN
     }
   };
 
-  // var url = "https://api.github.com/repos/" + repoOwner + repoName + "/contributors";
   request(options, function(err, res, body) {
+    body = JSON.parse(body);
+    // console.log(body);
     cb(err, body);
   });
+
 }
 
 getRepoContributors("aseprite", "aseprite", function(err, response) {
   if (err) throw err;
 
+  response.forEach(worker => {
+    console.log(worker.avatar_url);
+  });
   // console.log("Error:", err);
-  console.log("Result:", response);
+  // console.log("Result:", response);
 });
 
 console.log("Welcome to the Github Avatar Downloader!");
